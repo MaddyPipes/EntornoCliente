cec78991616002553794f290ee16c233753034db
MaddyPipes <alexisthinking@gmail.com>
2022-09-29 13:27:20 +0200

    Up!
    2º up

---
+++ Tema1/ejemplosTema1.js
@@ -0,0 +1,42 @@
+//Ejemplo alertas y logs.
+
+console.log("Hola Mundo!");
+
+alert("SERGIO DUCHATE Y DEJA EL WOW");
+
+//Ejemplo concatenación
+
+let edad = 23, nueva_edad, incremento;
+
+const nombre = "Rosa Garcia";
+
+incremento = 4;
+
+nueva_edad = edad + incremento;
+
+console.log(`${nombre} dentro de ${incremento} años tendrá ${nueva_edad}`);
+
+//Ejemplo arrays
+
+let miVector = [];
+
+miVector[0] = 1;
+miVector[1] = 2;
+miVector[2] = 3;
+miVector[3] = 4;
+miVector[4] = 5;
+miVector[5] = 6;
+miVector[6] = 7;
+miVector[7] = 8;
+miVector[8] = 9;
+miVector[9] = 10;
+
+let otroArray = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(miVector[3]);
+
+console.log(otroArray[5]);
+
+console.log(miVector);
+
+console.log(miVector.length);
