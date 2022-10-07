3c5c6b9f6813ed8b269d4c29ac61d203057c2e2d
MaddyPipes <alexisthinking@gmail.com>
2022-10-07 14:20:47 +0200

    asdf

--- Tema 2/ejemplosTema2Tanda2.js
+++ Tema 2/ejemplosTema2Tanda2.js
@@ -7,13 +7,13 @@ document.write("<h3>Ejercicio 8:</h3>");
 
 let randomNumber = Math.floor(Math.random() * 10) + 1;
 
-if (randomNumber === parseInt(prompt(`Introduce el número ${randomNumber}:`, ""))){
+if (randomNumber === parseInt(prompt(`Introduce el número ${randomNumber}:`, ""))) {
     alert("¡Buen trabajo!");
     document.write("El número coincide.")
 }
 else
     alert("No coincide");
-    document.write("El número no coincide.")
+document.write("El número no coincide.")
 
 // 9. Escriba un programa JavaScript para calcular los días que quedan hasta la
 // próxima Navidad.
@@ -41,12 +41,12 @@ let numA = parseInt(prompt("Introduce el primer número:"));
 let numB = parseInt(prompt("Introduce el segundo número:"));
 
 let ej10 = (a, b) => {
-    document.write(`Multiplicación de ambos números: ${numA*numB}`);
+    document.write(`Multiplicación de ambos números: ${numA * numB}`);
     document.write("<br>")
-    document.write(`División de ambos números: ${numA/numB}`);
- }
+    document.write(`División de ambos números: ${numA / numB}`);
+}
 
- ej10(numA, numB);
+ej10(numA, numB);
 
 //  11. Escriba un programa JavaScript para convertir temperaturas desde y hacia
 // Celsius, Fahrenheit.
@@ -57,15 +57,47 @@ let ej10 = (a, b) => {
 // 45 ° F es 7.222222222222222 ° C
 document.write("<h3>Ejercicio 11:</h3>");
 
-let temp = prompt("Introduce la temperatura: ");
+function tempUnitChanger(a) {
+    let localNumber = a.substring(0, a.length - 1);
+    let localUnit = a.slice(a.length - 1).toUpperCase();
+    if (localUnit == 'C') {
+        if ((localNumber * 1.8) + 32 == NaN) {
+            document.write("¡Formato de temperatura incorrecto!");
+        } else
+            document.write(`La temperatura dada, pasada a Fahrenheit es: ${(localNumber * 1.8) + 32}`);
+    } else if (localUnit == 'F') {
+        if (((localNumber - 32) / 1.8) == NaN) {
+            document.write("¡Formato de temperatura incorrecto!");
+        } else
+            document.write(`La temperatura dada, pasada a Celsius es: ${((localNumber - 32) / 1.8)}`);
+    } else {
+        document.write("¡Formato de temperatura incorrecto!");
+    }
+}
+
+let tempeUnit = prompt("Introduce la temperatura (Recuerda usar C para Celsius y F para Fahrenheit):");
+tempUnitChanger(tempeUnit);
+
+// 12. Escriba un programa JavaScript para obtener la URL del sitio web (página de
+//     carga).
+document.write("<h3>Ejercicio 12:</h3>");
+let actualURL = window.location;
+
+document.write(actualURL);
 
 // 13. Escriba un ejercicio de JavaScript para crear una variable con un nombre
 // definido por el usuario.
+document.write("<h3>Ejercicio 13:</h3>");
 
-let userVar = (window[prompt("Introduce un nombre para la variable.","")] = "buena esa");
+let userVar = (window[prompt("Introduce un nombre para la variable.", "")] = "buena esa");
 
 document.write(userVar);
- 
+
+// 14. Escriba un ejercicio de JavaScript para obtener la extensión de un nombre de
+// archivo.
+document.write("<h3>Ejercicio 14:</h3>");
+
+
 
 
 
